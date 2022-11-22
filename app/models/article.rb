6053d8e267c84dec3c519fe1c972355549defe47
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-22 10:37:53 +0400

    Add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  include Visible
+  
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :content, presence: true, length: { minimum: 2 }
+
+  has_many :comments, as: :commentable, dependent: :destroy
+  belongs_to :user
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  include Visible
+  
+  validates :body, presence: true, length: { maximum: 4000 }
+  
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true
 end
