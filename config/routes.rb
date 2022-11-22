938052132a52b72939bdb23afb1f085d89417701
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-22 13:27:56 +0400

    Add routes for articles and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get 'static_pages/about'
   get 'static_pages/contacts'
-
   root to: 'article#index'
+ 
+  shallow do
+    resources :users do
+      resource :artciles
+    end
+    resources :articles do
+      resource :comments
+    end
+  end
 end
