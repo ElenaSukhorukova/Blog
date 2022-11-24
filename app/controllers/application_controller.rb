90aef257b42e50848f70ead7e32886abf8afad8f
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-24 22:27:45 +0400

    Create helpers to models' name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :comment_model_name
+  helper_method :article_model_name
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  def comment_model_name
+    Comment.model_name.human
+  end
+  
+  def article_model_name
+    Article.model_name.human
   end
 end
