b96e6e71c4530c6c8848938e1e490e63ac6452be
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-03 17:52:26 +0400

    Update articles' controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
-  before_action :define_user, only: %i[new create]
-  before_action :define_article, except: %i[new create index]
+  before_action :define_user!, only: %i[new create]
+  before_action :define_article!, except: %i[new create index]
 
   def index
     if user_signed_in?
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
 
     if @article.save
       redirect_to articles_path, 
-        success: I18n.t('flash.new', model: article_model_name.downcase)
+        success: I18n.t('flash.new', model: i18n_model_name(@article).downcase)
     else
       render :new, status: :unprocessable_entity
     end
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
   def update
     if @article.update(article_params)
       redirect_to article_path(@article), 
-        success: I18n.t('flash.update', model: article_model_name.downcase)
+        success: I18n.t('flash.update', model: i18n_model_name(@article).downcase)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -52,17 +52,17 @@ class ArticlesController < ApplicationController
   def destroy
     if @article.destroy
       redirect_to articles_path, 
-        success: I18n.t('flash.destroy', model: article_model_name.downcase)
+        success: I18n.t('flash.destroy', model: i18n_model_name(@article).downcase)
     end
   end
 
   private
 
-    def define_user
+    def define_user!
       @user = User.find(params[:user_id]) if user_signed_in?
     end
 
-    def define_article
+    def define_article!
       @article = Article.find(params[:id])
     end
 
