db1384d2051de27962928323198bf3852dbadca4
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-14 13:48:08 +0400

    Add users' controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show 
+    @user = User.find(params[:id])
+    @user_pagy, @user_articles = pagy @user.articles.order(created_at: :desc), items: 4, page_param: :user_pagy
+    @user_articles = @user_articles.decorate
+  end
+end
