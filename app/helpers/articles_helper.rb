3b5f336dd307e922e6cba957c7a3d5bff6fc57b5
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-14 13:50:40 +0400

    Add helpers for articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,12 @@
 module ArticlesHelper
+  def count_comments(article)
+    return article.comments.where.not(status: Article::VALID_STATUES[2]).count if current_user
+
+    article.comments.where(status: Article::VALID_STATUES[0]).count
+  end
+
+  def anchor(article)
+    visible = user_signed_in? ? article.comments.pablic_private : article.comments.pablic
+    dom_id(visible.first)
+  end
 end
