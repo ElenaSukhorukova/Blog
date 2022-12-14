2ca0d11d9dd9b10f8269ac9415951973000e2ef1
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-14 13:48:55 +0400

    Add concern for lookinf for 5 most commented articles

---
+++ app/controllers/concerns/most_articles.rb
@@ -0,0 +1,12 @@
+module MostArticles
+  extend ActiveSupport::Concern
+  def most_commented(old_articles)
+    articles = []     
+    old_articles.order(created_at: :asc).each do |article|
+          if old_articles.all.map(&:comments_count).max(5).include?(article.comments_count)
+            articles << Article.find_by(id: article.id)
+          end
+      return articles if articles.size == 5
+    end
+  end
+end
