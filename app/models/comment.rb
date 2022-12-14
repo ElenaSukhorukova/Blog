1e6aaea1121008ddc965b4728a53a2975da1904b
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-14 13:52:07 +0400

    Add scopes for a status of a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,7 @@ class Comment < ApplicationRecord
   
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  scope :pablic_private, -> { where.not(status: Comment::VALID_STATUES[2]).order(created_at: :desc) }
+  scope :pablic, -> { where(status: Comment::VALID_STATUES[0]).order(created_at: :desc) }
 end
