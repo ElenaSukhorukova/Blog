06fcd6d91ea460564fd3bcb23d8d12c6a5493837
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-14 13:47:05 +0400

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   include ErrorHandling
   include ApplicationHelper
+  include MostArticles
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :define_articles!
 
   add_flash_types :info, :danger, :warning, :success, :alert, :notice
 
@@ -10,5 +12,20 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
+
+    def define_articles!
+      if user_signed_in?
+        @main_pagy, @articles = pagy Article.pablic_private, items: 4, page_param: :main_pagy  
+        @comentered_articles = most_commented(@articles)
+        @comentered_articles = @comentered_articles.map(&:decorate)
+        @articles = @articles.decorate
+  
+        @sidebar_pagy, @archived_articles = pagy Article.pablic, items: 4, page_param: :sidebar_pagy
+        @archived_articles = @archived_articles.decorate
+      else
+        @main_pagy, @articles = pagy Article.archived, items: 4, page_param: :main_pagy
+        @articles = @articles.decorate
+      end
     end
 end
