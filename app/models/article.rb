fb0f7c77b7b4c79633a871a514a35467bbf85e33
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-14 13:51:38 +0400

    Update article's model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,20 @@
 class Article < ApplicationRecord
   include Visible
-  include TimeShow
 
   validates :title, presence: true, length: { minimum: 2 }
   validates :content, presence: true, length: { minimum: 2 }
 
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :pablic_private, -> { pablic_private }, class_name: 'Comment'
+  has_many :pablic, -> { pablic }, class_name: 'Comment'
+
   belongs_to :user
+
+  def comments_count
+    self.comments.count
+  end
+
+  scope :pablic_private, -> { where.not(status: Article::VALID_STATUES[2]).order(created_at: :desc) }
+  scope :pablic, -> { where(status: Article::VALID_STATUES[0]).order(created_at: :desc) }
+  scope :archived, -> { where(status: Article::VALID_STATUES[2]).order(created_at: :desc) }
 end
