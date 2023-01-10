99677009ae87cef11c58f98c08afc04582313a82
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-10 14:05:59 +0400

    Add concern and include Internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include ErrorHandling
   include ApplicationHelper
   include MostArticles
+  include Internationalization
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :define_articles!
 
