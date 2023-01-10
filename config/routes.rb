a7e29e97d6042b252793b95446f05b6090a4f12b
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-10 20:11:02 +0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    devise_for :users
 
-  devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
-    get 'sign_up', to: 'devise/registrations#new'
-    delete 'sign_out', to: 'devise/sessions#destroy'
-  end
-
-  get 'static_pages/about'
-  get 'static_pages/contacts'
-  root to: 'articles#index'
-
-  shallow do
-    resources :users, only: %i[show] do
-      resources :articles
+    devise_scope :user do
+      get 'sign_in', to: 'devise/sessions#new'
+      get 'sign_up', to: 'devise/registrations#new'
+      delete 'sign_out', to: 'devise/sessions#destroy'
     end
-    resources :articles do
-      resources :comments
+
+    get 'static_pages/about'
+    get 'static_pages/contacts'
+    root to: 'articles#index'
+
+    shallow do
+      resources :users, only: %i[show] do
+        resources :articles
+      end
+      resources :articles, only: %i[index] do
+        resources :comments
+      end
     end
   end
-
-  resources :articles, only: %i[index]
 end
