78d50502936d83e3e060b815fe988e2f71ede9af
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-11 14:11:52 +0400

    Add permit for images

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -62,6 +62,6 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :content, :status)
+    params.require(:article).permit(:title, :content, :status, images: [])
   end
 end
